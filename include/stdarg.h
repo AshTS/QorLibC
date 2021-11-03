25ea6185b566de4cbc450d26181a5dfea894b659
CarterTS <carterplasek@gmail.com>
2021-11-03 17:27:32 -0500

    Change stdarg.h to use the builtin implementation

--- include/stdarg.h
+++ include/stdarg.h
@@ -1,15 +1,17 @@
 #ifndef _STDARG_H
 #define _STDARG_H
 
-typedef char* va_list;
+#include "stddef.h"
 
-#define _internal_va_size(v) (((sizeof(v) + sizeof(int) - 1) / (sizeof(int))) * (sizeof(int)))
+typedef __builtin_va_list va_list;
 
-#define va_start(ap, var) ((ap) = ((va_list)&var - _internal_va_size(var)))
+#define internal_va_size(v) (((sizeof(v) + sizeof(int) - 1) / (sizeof(int))) * (sizeof(int)))
 
-#define va_arg(ap, type) (((ap) = (ap - _internal_va_size(type))), (*((type*) (ap + _internal_va_size(type)))))
+#define va_start(ap, var) __builtin_va_start(ap, var)
 
-#define va_end(ap) ((void)0)
+#define va_arg(ap, type) __builtin_va_arg(ap, type)
+
+#define va_end(ap) __builtin_va_end(ap)
 
 
 #endif // _STDARG_H
