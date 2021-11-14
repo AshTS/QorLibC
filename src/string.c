d1c17f23fa873b9941004c11975fa431789e08a6
CarterTS <carterplasek@gmail.com>
2021-11-13 23:19:35 -0600

    string.c: Correct Behaviors

--- src/string.c
+++ src/string.c
@@ -189,6 +189,11 @@ int strcmp(const char* s1, const char* s2)
         }
     }
 
+    if (*s1 != *s2)
+    {
+        return *s1 - *s2;
+    }
+
     return 0;
 }
 
@@ -405,7 +410,7 @@ char* strerror(int error)
 size_t strlen(const char* s)
 {
     size_t i = 0;
-    for (;s[i] == 0;i++);
+    for (;s[i] != 0;i++);
 
     return i;
 }
